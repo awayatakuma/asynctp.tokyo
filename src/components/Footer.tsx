e582a1f0592e07637429ea96d0d321795bcc3112
awayatakuma <takuma.awaya@gmail.com>
2024-05-19 08:56:24 +0900

    add copyright

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,25 @@
-import { Flex } from '@chakra-ui/react'
+import { Flex, Spacer, Text } from '@chakra-ui/react'
 import { SocialLinks } from '@/components'
 
 export const Footer: React.FC = () => (
   <Flex
     bottom={0}
     as="footer"
-    direction={{ base: 'row' }}
+    direction={{ base: 'column' }}
     alignItems={{ base: 'start', md: 'center' }}
-    pb={8}
     pt={8}
+    mb={4}
     px={{ base: 4, md: 8 }}
     mt="auto"
-    gap={10}
   >
     <SocialLinks />
+    <Flex w="full" alignItems="center" mt={1}>
+      {' '}
+      <Spacer />
+      <Text as="span" fontSize="sm">
+        ©asynctp
+      </Text>
+      <Spacer />
+    </Flex>
   </Flex>
 )
