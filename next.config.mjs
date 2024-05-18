599f61c19a6cda4849752c014e5e9a0edef7c696
awayatakuma <takuma.awaya@gmail.com>
2024-05-18 14:22:51 +0900

    fix the problem that submodules use CommonJS import style
    Refert to
    https://github.com/vercel/next.js/issues/64434#issuecomment-2082964050

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    optimizePackageImports: ['shiki', 'next-mdx-remote/rsc'],
+  },
+}
 
 export default nextConfig
