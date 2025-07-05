bfe48a8a85ddee0fb00d707fcbb6872f860ddf64
awayatakuma <takuma.awaya@gmail.com>
2025-07-05 17:54:24 +0900

    change lights

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import { Box, Image } from '@chakra-ui/react'
 import { VRMViewer } from '@/components'
 
-export default async function Top() {
+export default function Top() {
   return (
     <Box position="relative" width="100%" height="80vh">
       {/* 背景画像 */}
@@ -17,7 +19,7 @@ export default async function Top() {
         zIndex="1"
       />
 
-      {/* 前面のVRMViewer */}
+      {/* 前面のVRMViewer - ゆっくりフェードイン */}
       <Box
         position="absolute"
         top="0"
@@ -25,6 +27,13 @@ export default async function Top() {
         width="100%"
         height="100%"
         zIndex="10"
+        animation="delayedFadeIn 3s ease-in-out 1s both"
+        sx={{
+          '@keyframes delayedFadeIn': {
+            '0%': { opacity: 0 },
+            '100%': { opacity: 1 },
+          },
+        }}
       >
         <VRMViewer
           vrmUrl="/assets/asynct_low.vrm"
