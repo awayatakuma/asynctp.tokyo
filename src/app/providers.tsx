bf502525aee10df9c3d41639ea3172c80c46315c
awayatakuma <takuma.awaya@gmail.com>
2025-07-12 11:28:51 +0900

    remove depreciated code

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,15 +1,14 @@
 'use client'
 
-import { CacheProvider } from '@chakra-ui/next-js'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Fonts } from '@/components/Fonts'
 import theme from '@/theme'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <CacheProvider>
+    <>
       <Fonts />
       <ChakraProvider theme={theme}>{children}</ChakraProvider>
-    </CacheProvider>
+    </>
   )
 }
